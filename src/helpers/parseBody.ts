0f9bdb7d78c72975bf7e897b01c600c1be922032
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-05 16:53:27 +0600

    feat: add parseBody unility for handing incoming JSON request data

---
+++ src/helpers/parseBody.ts
@@ -0,0 +1,19 @@
+import { resolve } from "node:dns";
+import { IncomingMessage } from "node:http";
+
+async function parseBody(req: IncomingMessage): Promise<any> {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // convert buffer to string
+    });
+
+    req.on("end", () => {
+      try {
+        resolve(body ? JSON.parse(body) : {});
+      } catch (err: any) {
+        reject(err);
+      }
+    });
+  });
+}
