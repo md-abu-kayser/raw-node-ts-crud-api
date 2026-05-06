7d2c45b3e7dea44e4dd42ebd5d326061ea22c840
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-06 12:32:40 +0600

    feat: add sendJson utility function

--- src/helpers/sendJson.ts
+++ src/helpers/sendJson.ts
@@ -1,8 +1,11 @@
 import { ServerResponse } from "http";
 
 function sendJson(res: ServerResponse, statusCode: number, data: unknown) {
-  res.writeHead(statusCode, { "content-type": "application/json" });
-  res.end(JSON.stringify(data));
+  res.writeHead(statusCode, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
+
+  res.end(JSON.stringify(data, null, 2));
 }
 
 export default sendJson;
